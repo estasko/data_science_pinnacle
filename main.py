a735bbe123d0e37dbb1148d219f492acecc8f6af
Abbyhow <abbyjh.002@gmail.com>
2025-03-26 15:41:58 -0400

    check nan and zero

--- main.py
+++ main.py
@@ -9,8 +9,12 @@ Created on Wed Mar 26 14:48:37 2025
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+#%%
+#read in dataset
+data = pd.read_csv('Healthcare-Diabetes.csv', index_col= 'Id')
 
-data = pd.read_csv('Healthcare-Diabetes.csv')
-
+#check for na 
 data.isna().any()
-
+#%%
+#see how many zero counts for each column
+(data == 0).sum()
