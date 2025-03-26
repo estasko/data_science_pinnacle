516c4542c61d10aa6be16cf6a11e1f50092d8c5b
Abbyhow <abbyjh.002@gmail.com>
2025-03-26 15:18:22 -0400

    read data

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 26 14:48:37 2025
 
-'''
-This is a test to see if I can write in spyder and push to github
-'''
-#hello
+@author: abbyhoward
+"""
+
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+data = pd.read_csv('Healthcare-Diabetes.csv')
+
+data.isna().any()
+
