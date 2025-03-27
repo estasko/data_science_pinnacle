349e2e024fb938e6668fd4bd941032f8468714a9
aalubas <arailubas35@gmail.com>
2025-03-26 22:47:55 -0400

    Scatter Plots & Histograms

--- main.py
+++ main.py
@@ -35,11 +35,12 @@ for col in data:
         dataframes[col] = data.loc[:, (col, 'Outcome')].query('`{}` != 0'.format(col))
 
 dataframes['Pregnancies'].head()
-#%%
+#%% Clean Data
 # Cleaned data with no zero values for any row 
 cleaned_data = data.query('Glucose !=0 & BloodPressure !=0 & SkinThickness' 
                           '!=0 & Insulin !=0')
-#%%
+
+#%% Plot Violin Plots
 plt.figure(1, figsize = (8,6), clear = True)
 
 subplot_num = 1
@@ -52,7 +53,59 @@ for key in dataframes:
 
 plt.tight_layout()
 
+#%% Plot Scatter Plots
 
+#Graph various variables vs. 2 Hour Serum Insulin 
+plt.figure(2, clear = True)
+plt.subplot(1,3, 1)
 
+sns.scatterplot(x = 'BMI',
+           y = 'Insulin',
+           data = cleaned_data,
+           hue = 'Outcome',
+           size = 2)
 
+plt.title('BMI vs. 2-Hour Serum Insulin')
+plt.xlabel('BMI')
+plt.ylabel('2-Hour Serum Insulin mu U/mL')
 
+plt.subplot(1,3, 2)
+sns.scatterplot(x = 'Glucose',
+           y = 'Insulin',
+           data = cleaned_data,
+           hue = 'Outcome',
+           size = 2)
+
+plt.title('Glucose vs 2-Hour Serum Insulin')
+plt.xlabel('Glucose')
+plt.ylabel('2-Hour Serum Insulin mu U/mL')
+
+plt.subplot(1,3, 3)
+sns.scatterplot(x = 'Pregnancies',
+           y = 'Insulin',
+           data = cleaned_data,
+           hue = 'Outcome',
+           size = 2 )
+
+plt.title('Pregnancies vs 2-Hour Serum Insulin')
+plt.xlabel('Pregnancies')
+plt.ylabel('2-Hour Serum Insulin mu U/mL')
+
+#%% Exploring the Data
+plt.figure(3, clear = True)
+sns.histplot(cleaned_data,
+             x = 'Insulin',
+             hue = 'Outcome')
+plt.title('2-Hour Insulin Serum by Diabetes Diagnosis')
+
+plt.figure(4, clear = True)
+sns.histplot(cleaned_data, 
+              x = 'Age')
+plt.title('Age of Participants in Dataset')
+
+plt.figure(5, clear = True)
+sns.histplot(cleaned_data, 
+              x = 'Outcome',
+              hue = 'Outcome',
+              bins = 2)
+plt.title('Diagnosis Count Among Participants')
